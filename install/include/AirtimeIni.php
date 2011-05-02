287c6ac2743351619fe620eae1a0535dc0f61cba
Naomi <naomi@naomi-DX4840.(none)>
2011-05-02 13:41:21 -0400

    CC-2219 : Airtime ubuntu packages overwrites configuration files

--- install/include/AirtimeIni.php
+++ install/include/AirtimeIni.php
@@ -181,7 +181,7 @@ class AirtimeIni
         $lines = file($p_filename);
         $n=count($lines);
         for ($i=0; $i<$n; $i++) {
-            if (!in_array(substr($lines[$i], 0, 1). array('#', ''))){
+            if (!in_array(substr($lines[$i], 0, 1), array('#', ''))){
                 $info = explode("=", $lines[$i]);
                 $values[trim($info[0])] = trim($info[1]);
             }
